f6627609fd12a4d66d2145d01e553b9387227b0a
Shaw Coney <rockconey@gmail.com>
2023-03-02 17:35:09 -0600

    Part 5 complete.

---
+++ script.js
@@ -0,0 +1,34 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+let userClickedPattern = [];
+
+$(".btn").click(function() {
+
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  
+  playSound(userChosenColour);
+
+});
+
+function nextSequence() {
+
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+ 
+  playSound(randomChosenColour);
+}
+
+
+function playSound(name) {
+
+ 
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
